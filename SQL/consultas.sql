ef069bdb8d643a49acb5c30c1121651308ee681b
Relezcano <relezcano369@gmail.com>
2017-11-22 20:58:43 -0300

    Actualizacion

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
--- AGREGAR REGISTRO DE USUARIO
-insert into Users (name,lastName, phone, password, email, admin, prom)values ('Facundo', 'Sautú', 2944824075, 'admin', 'facundo.sautu@gmail.com', 1, 0);
-=======
 -- AGREGAR REGISTRO DE USUARIO 
-insert into Users (name,lastName, phone, password, email, admin, prom)values ('Facundo', 'Sautú', 2944824075, 'admin', 'facundo.sautu@gmail.com', 1, 0); 
->>>>>>> a0322e6ed78231710776962a79f2a7e27ece8338
+INSERT INTO Users (name,lastName, phone, password, email, admin, prom) VALUES ('Facundo', 'Sautú', 2944824075, 'admin', 'facundo.sautu@gmail.com', 1, 0); 
 
 -- BORRAR USUARIO
 
@@ -18,19 +13,12 @@ select password, email from Users where email="relezcano369@gmail.com" and passw
 -- Alterar tabla
 alter table Users add column (prom boolean);
 
-<<<<<<< HEAD
-delete from Users where id=4;
-
--- Consulta general
-
-select id from tabAct where Col = Val;
-
-select * from 
-=======
 delete from Users where id=4; 
 
 -- Consulta general
 
 select id from tabAct where Col = Val; 
 
->>>>>>> a0322e6ed78231710776962a79f2a7e27ece8338
+select * from Users;
+
+
--- include/registro_usuario.php
+++ include/registro_usuario.php
@@ -1,50 +1,55 @@
 <?php
-$db_host="localhost";
-$db_user="root";
-$db_password="root";
-$db_name="vinos";
-$db_table_name="Users";
-$db_connection = mysqli_connect($db_host, $db_user, $db_password);
+session_start();
 
-if (!$db_connection) {
-	die('No se ha podido conectar a la base de datos');
-}
-$form_name = ($_POST['name']);
-$form_last = ($_POST['lastName']);
-$form_phone = ($_POST['phone']);
-$form_email = ($_POST['email']);
-$form_pass = ($_POST['password']);
-$form_rpass = ($_POST['rpassword']);
-$form_prom = ($_POST['prom']);
-$form_admin = '1';
+require 'conexion.php';
 
-$resultado = mysql_query("SELECT * FROM ".$db_table_name." WHERE Email = '".$form_email."'", $db_connection);
+if(isset($_POST['registrar'])) {
+  if($_POST['name'] == '' or $_POST['email'] == '' or $_POST['password'] == '' or $_POST['repassword'] == '') {
+    echo 'Por favor complete todos los campos para poder continuar.';//Si los campos están vacíos muestra el siguiente mensaje, caso contrario sigue el siguiente codigo.
+  }else {
+    $query = 'SELECT * FROM Users';
+    $rec = mysqli_query($link, $query);
+    $verificar_usuario = 0;//Creamos la variable $verificar_usuario que empieza con el valor 0 y si la condición que verifica el usuario(abajo), entonces la variable toma el valor de 1 que quiere decir que ya existe ese nombre de usuario por lo tanto no se puede registrar
 
-if (mysql_num_rows($resultado)>0)
-{
+    while($resultado = mysqli_fetch_object($rec)) {
+      if($resultado->email == $_POST['email']) { //Esta condición verifica si ya existe el usuario
+        $verificar_usuario = 1;
+      }
+    }
 
-echo "Registro fallido";
-// header('Location: Fail.html');
+    if($verificar_usuario == 0)
+      {
+        if($_POST['password'] == $_POST['repassword'])//Si los campos son iguales, continua el registro y caso contrario saldrá un mensaje de error.
+      {
+          $form_name = ($_POST['name']);
+          $form_last = ($_POST['lastName']);
+          $form_phone = ($_POST['phone']);
+          $form_email = ($_POST['email']);
+          $form_pass = ($_POST['password']);
+          $form_rpass = ($_POST['repassword']);
+          $form_admin = 0;
 
-} else {
+          if ($_POST['prom'] == "on") {
+            $form_prom = 1;
+          } else {
+            $form_prom = 0;
+          }
 
-  if (($_POST['password'])==($_POST['rpassword'])) {
+          $sql = "INSERT INTO Users (name, lastName, phone, password, email, admin, prom) VALUES ('$form_name', '$form_last', '$form_phone', '$form_pass', '$form_email', '$form_admin', '$form_prom')";//Se insertan los datos a la base de datos y el usuario ya fue registrado con exito.
+          mysqli_query($link, $sql);
 
-    $insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`name` , `lastName` , `phone`, `password`, `email`, `admin`, `prom`) VALUES ("' . $form_name . '", "' . $form_last . '", "' . $form_phone . '", "' . $form_pass . '", "' . $form_email . '", "' . $form_admin . '", "' . $form_prom . '")';
+          echo $sql;
 
-  mysqli_select_db($db_name, $db_connection);
-  $retry_value = mysqli_query($insert_value, $db_connection);
-
-  if (!$retry_value) {
-     die('Error: ' . mysql_error());
+          echo 'Usted se ha registrado correctamente.<br>';
+          echo 'Para volver al inicio <a href="../index.php">Haga click aquí</a>';
+        }else {
+          echo 'Las claves no son iguales, intente nuevamente.';
+        }
+      }else {
+        echo 'Este usuario ya ha sido registrado anteriormente.';
+      }
+    }
   }
-
-  echo "Registro exitoso";
-  // header('Location: Success.html');
-  }
-
-  mysql_close($db_connection);
-  }
-
+  mysqli_close($link);
 
 ?>
